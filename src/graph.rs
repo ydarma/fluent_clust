649a8dbac98ebbb523d6ea6a05ee9d3fbb7019d5
Yves Darmaillac <ydarma@proton.me>
2022-08-29 09:58:08 +0200

    graph API enhanced

--- src/graph.rs
+++ src/graph.rs
@@ -3,7 +3,7 @@ use std::{
     rc::{Rc, Weak},
 };
 
-struct Vertex<Data> {
+pub(crate) struct Vertex<Data> {
     node: Rc<RefCell<Node<Data>>>,
 }
 
@@ -19,35 +19,52 @@ impl<Data> Vertex<Data> {
     }
 
     pub(crate) fn first(mean: Data) -> Vertex<Data> {
-        Self::new(mean, (None, None))
+        Self::new(mean, [None, None])
     }
 
-    pub(crate) fn new(data: Data, neighbors: (Neighbor<Data>, Neighbor<Data>)) -> Vertex<Data> {
+    pub(crate) fn new(data: Data, neighbors: [Neighbor<Data>; 2]) -> Vertex<Data> {
         Vertex {
             node: Rc::new(RefCell::new(Node { data, neighbors })),
         }
     }
 
-    fn first_neighbor(&self) -> Option<Vertex<Data>> {
-        Vertex::as_vertex(&self.node.borrow().neighbors.0)
+    pub(crate) fn edges(&self) -> impl Iterator<Item = Vertex<Data>> + '_ {
+        Edges::new(Ref::map(self.node.borrow(), |n| &n.neighbors))
     }
 
-    fn second_neighbor(&self) -> Option<Vertex<Data>> {
-        Vertex::as_vertex(&self.node.borrow().neighbors.1)
-    }
-
-    fn get_data(&self) -> Ref<'_, Data> {
+    pub(crate) fn get_data(&self) -> Ref<'_, Data> {
         Ref::map(self.node.borrow(), |n| &n.data)
     }
 
-    fn set_data(&self, data: Data) {
+    pub(crate) fn set_data(&self, data: Data) {
         self.node.borrow_mut().data = data;
+    }
+}
+
+struct Edges<'a, Data> {
+    curr: usize,
+    neighbors: Ref<'a, [Neighbor<Data>; 2]>,
+}
+
+impl<'a, Data> Iterator for Edges<'a, Data> {
+    type Item = Vertex<Data>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let vertex = Vertex::as_vertex(&self.neighbors[self.curr]);
+        self.curr += 1;
+        vertex
+    }
+}
+
+impl<'a, Data> Edges<'a, Data> {
+    fn new(neighbors: Ref<'a, [Neighbor<Data>; 2]>) -> Self {
+        Edges { curr: 0, neighbors }
     }
 }
 
 pub struct Node<Data> {
     pub data: Data,
-    pub neighbors: (Neighbor<Data>, Neighbor<Data>),
+    pub neighbors: [Neighbor<Data>; 2],
 }
 
 impl<Point> Node<Point> {}
@@ -59,20 +76,21 @@ mod tests {
     #[test]
     fn test_build_node() {
         let n1 = Vertex::first(1);
-        let n2 = Vertex::new(2, (n1.as_neighbor(), None));
-        let n3 = Vertex::new(3, (n1.as_neighbor(), n2.as_neighbor()));
-        assert_eq!(n1.node.as_ptr(), n3.first_neighbor().unwrap().node.as_ptr());
+        let n2 = Vertex::new(2, [n1.as_neighbor(), None]);
+        let n3 = Vertex::new(3, [n1.as_neighbor(), n2.as_neighbor()]);
+        let mut e3 = n3.edges();
+        assert_eq!(n1.node.as_ptr(), e3.next().unwrap().node.as_ptr());
         assert_eq!(
             n2.node.as_ptr(),
-            n3.second_neighbor().unwrap().node.as_ptr()
+            e3.next().unwrap().node.as_ptr()
         );
     }
 
     #[test]
     fn test_graph_mutation() {
         let n1 = Vertex::first(1);
-        let n2 = Vertex::new(1, (n1.as_neighbor(), None));
-        let n1_from_n2 = n2.first_neighbor().unwrap();
+        let n2 = Vertex::new(1, [n1.as_neighbor(), None]);
+        let n1_from_n2 = n2.edges().next().unwrap();
         n1_from_n2.set_data(3);
         assert_eq!(3, *n1.get_data());
     }
@@ -80,9 +98,9 @@ mod tests {
     #[test]
     fn test_vertex_suppression() {
         let n1 = Vertex::first(1);
-        let n2 = Vertex::new(2, (n1.as_neighbor(), None));
+        let n2 = Vertex::new(2, [n1.as_neighbor(), None]);
         let mut graph = vec![n1, n2];
         graph.remove(0);
-        assert!(graph[0].first_neighbor().is_none());
+        assert!(graph[0].edges().next().is_none());
     }
 }
